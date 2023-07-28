93e859c8cd4895841739ae541517f826fdf01213
quyctn-a3mk <quyctn.a3mk@gmail.com>
2023-07-28 10:08:17 +0700

    add algo directory: base, ftg, tg

---
+++ FTG/src/algorithm/tg.py
@@ -0,0 +1,36 @@
+import random
+from copy import deepcopy
+import argparse
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type, TypeVar, Union
+
+from common.lib.sgraph import SGraph
+from utils.basealgo import BaseAlgorithm
+
+MODEL: List[str] = ["IC", "LT",]
+METHOD : List[str] = ["RIS", "MC"]
+DEFAULT_NUM_SAMPLE = 1e2
+MAX_NUM_SAMPLE = 1e8
+DEFAULT_NUM_PARALLEL_THREAD = 1
+MAX_NUM_PARALLEL_THREAD = 10
+
+SelfBaseAlgorithm = TypeVar("SelfBaseAlgorithm", bound="BaseAlgorithm")
+
+class GreedyMCSC(BaseAlgorithm):
+    def __init__(
+		self, 
+		**kwargs
+	) -> None:
+        super().__init__(**kwargs)
+        pass
+    def run(self,):
+        pass
+    
+class GreedyRevenue(BaseAlgorithm):
+    def __init__(
+		self, 
+		**kwargs
+	) -> None:
+        super().__init__(**kwargs)
+        pass
+    def run(self,):
+        pass
